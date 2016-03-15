45ba2a11263ebdc24a7d9205c4bec5cc125779d4
thaiquang <thaiquang101246@gmail.com>
2016-03-15 12:39:45 +0700

    add new icon game

--- js/game.js
+++ js/game.js
@@ -17,8 +17,8 @@ for(var i = 0; i < 16;i++){
   $('.none').eq(i)
 }
 
-var snd_start = new Audio("sound/BMG03.mp3");
-var snd_bg = new Audio("sound/BMG01.mp3");
+var snd_start = new Audio("sound/BGM03.mp3");
+var snd_bg = new Audio("sound/BGM01.mp3");
 var snd_click = new Audio("sound/button_click.mp3");
 var snd_choose_wrong = new Audio("sound/choose_wrong.mp3");
 var snd_choose_right = new Audio("sound/QnA_right.mp3");
